29eab72a570d710ce652400c180ce35d96d12ea2
mahasagheer <mahasagheer960@gmail.com>
2024-06-01 15:57:59 -0700

    initial

---
+++ API Fetch/api/src/App.jsx
@@ -0,0 +1,14 @@
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// import ViewPost from "./pages/Viewpost";
+// import FetchPost from "./components/FetchPost";
+
+function App() {
+  return (
+    <>
+      <div className="h-20 w-20 bg-slate-400">hi</div>
+    </>
+  );
+}
+
+export default App;
